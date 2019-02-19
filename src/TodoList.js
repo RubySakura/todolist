435c5c5cde3762aa2a2201fb8ae116ed803b7e42
jian.li <ruby_sakura@qq.com>
2019-02-19 17:20:37 +0800

    优化TodoList代码

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './style.css';
 import TodoItem from './TodoItem';
+import './style.css';
 
 class TodoList extends Component {
   constructor(props) {
@@ -18,46 +18,54 @@ class TodoList extends Component {
     return (
       <>
         <div>
-          <input type="text" value={this.state.inputValue} onChange={this.handleChange} className='input' />
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleChange}
+            className='input'
+          />
           <button onClick={this.handleButtnClick}>提交</button>
         </div>
         <ul>
-          {
-            this.state.list.map((item, index) => {
-              // return (<li key={index} onClick={this.handleDelete.bind(this, index)}>{item}</li>);
-              return (
-                <TodoItem
-                  content={item}
-                  key={index}
-                  index={index}
-                  deleteItem={this.handleDelete}
-                />
-              )
-            })
-          }
+          {this.getTodoItem()}
         </ul>
       </>
     );
   }
 
-  handleChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    });
-  }
-
-  handleButtnClick() {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
-      inputValue: ''
+  getTodoItem() {
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem
+          content={item}
+          key={index}
+          index={index}
+          deleteItem={this.handleDelete}
+        />
+      )
     })
   }
 
+  handleChange(e) {
+    // 此处存在一个疑问，为什么不可以直接使用e
+    const value = e.target.value; 
+    this.setState((prevState) => ({
+      inputValue: value
+    }));
+  }
+
+  handleButtnClick() {
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.inputValue],
+      inputValue: ''
+    }));
+  }
+
   handleDelete(index) {
-    const list = [...this.state.list];
-    list.splice(index, 1);
-    this.setState({
-      list: list
+    this.setState((prevState) => {
+      const list = [...this.state.list];
+      list.splice(index, 1);
+      return {list};
     });
   }
 }
